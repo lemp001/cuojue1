a13946441540cc2933a30e9d752c40756a32c7a6
lemp001 <316882269@qq.com>
2019-04-24 23:51:10 +0800

    save

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -4,17 +4,54 @@ import store from '../../store'
 class Home extends Component {
   constructor (props) {
     super (props)
+    this.state=store.getState()
+    store.subscribe(this.handleStoreChange)
   }
   render () {
+    console.log('this.state',this.state)
     return (
-      <>
+      <div style={{padding: 20}}>
         <div>234</div>
         <div>456</div>
         <div>789</div>
         <div>今天下雨</div>
-      </>
-      
+        <input 
+          value={this.state.inputValue}
+          onChange={(e) => this.handleInputChange(e.target.value)}  
+        />
+        <button
+          style={{paddingLeft: 20}}
+          onClick={this.handleClickChange}
+        >按钮</button>
+        {
+          this.state.list.map((item, index) => <div key={index} onClick={() => this.handleDelete(index)}>{item}</div>)
+        }
+      </div>
     )
+  }
+  handleInputChange = (value) => {
+    const action = {
+      type: 'input_change_value',
+      inputValue: value
+    }
+    store.dispatch(action)
+  }
+  handleClickChange = () => {
+    const action = {
+      type: 'button_click_change',
+      value: this.state.inputValue
+    }
+    store.dispatch(action)
+  }
+  handleDelete = (index) => {
+    const action = {
+      type: 'delete_div',
+      index,
+    }
+    store.dispatch(action)
+  }
+  handleStoreChange = () => {
+    this.setState(store.getState())
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,5 +4,19 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+  if(action.type === 'input_change_value') {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.inputValue;
+    return newState;
+  }else if(action.type === 'button_click_change') {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(action.value)
+    newState.inputValue = ''
+    return newState
+  }else if(action.type === 'delete_div') {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index, 1)
+    return newState
+  }
   return state;
 }
