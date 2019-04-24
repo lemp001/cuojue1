cb9465c61131201cbcae22146ad258e51d064f50
lemp001 <lemp001@lemp001>
2019-04-24 17:21:32 +0800

    create

---
+++ src/components/home/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import store from '../../store'
+
+class Home extends Component {
+  constructor (props) {
+    super (props)
+  }
+  render () {
+    return (
+      <div>234</div>
+    )
+  }
+}
+
+export default Home
