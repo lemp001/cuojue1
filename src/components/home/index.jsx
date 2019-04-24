9e48fbbafbe6f134e794bf548471525eba2bec9f
lemp001 <lemp001@lemp001>
2019-04-24 17:37:29 +0800

    c

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -7,7 +7,12 @@ class Home extends Component {
   }
   render () {
     return (
-      <div>234</div>
+      <>
+        <div>234</div>
+        <div>456</div>
+        <div>789</div>
+      </>
+      
     )
   }
 }
